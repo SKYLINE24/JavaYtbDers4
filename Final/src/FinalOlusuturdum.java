f948ddb8a27997ec8a0d6e2af34430d7785d3ae8
Erbil Can Keleş <cankls2434@gmail.com>
2022-08-23 15:54:50 +0300

    değişken değer atamaları yapıldı ama sadece Final değişkeni örneği yazıldı

---
+++ Final/src/FinalOlusuturdum.java
@@ -0,0 +1,12 @@
+public class FinalOlusuturdum {
+    public final int gizliBilgi = 23;
+
+    /*public FinalTest(){
+        gizliBilgi = 12;
+    }
+    public void Deneme(){
+        gizliBilgi = 20;
+    }*/
+    //değiştirilemediği için hata veriyor
+
+}
